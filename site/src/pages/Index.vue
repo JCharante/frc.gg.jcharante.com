e4675939349f419f362bd700d24016255541e187
jcharante <git@jcharante.com>
2019-03-01 23:29:33 -0500

    initial commit

---
+++ site/src/pages/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page>
+    <div class="row flex flex-center">
+      <h1>frc.gg</h1>
+    </div>
+    <div class="row flex flex-center">
+      <p>Ever wanted op.gg but for FRC? No? Well too bad because I already made it.</p>
+    </div>
+    <div class="row flex flex-center">
+      <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    </div>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+};
+</script>
---
+++ site/src/router/routes.js
@@ -0,0 +1,21 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/team/:team', component: () => import('pages/Team.vue') },
+    ],
+  },
+];
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue'),
+  });
+}
+
+export default routes;
