1f23f6651a784c2ad2954103fdacf65fee64cfea
jcharante <git@jcharante.com>
2019-03-08 00:57:29 -0500

    update ranking system explanation

--- site/src/pages/RankingSystem.vue
+++ site/src/pages/RankingSystem.vue
@@ -6,12 +6,18 @@
     the matches for the event. Then each match is individually processed according
     to its timestamp.</p>
     <p>
-      When each team is imported into the script, it is given a default ranking of 1500,
-      which is roughly Gold III. I use the TrueSkill library
+      When each team is imported into the script, it is given a default ranking of 1500.
+      I use the TrueSkill library
       with a mu of 1500, a sigma of 500, and a beta of 250.
       Currently, each match is a 3v3, and only the match outcome (win, lose, or tie) is taken
       into account. In the future, we hope to take into account different attributes from the
       match to generate higher quality rankings.
+    </p>
+    <p>
+      Titles are calculated from the team's mu value at the time of the match
+      against percentile thresholds. This means that at the very first match
+      of the season, everyone in the match should be Challenger, because everyone
+      should have an mu value of 1500. Over the next matches, this quickly normalizes.
     </p>
     <p>
       Finally, the script exports all its data into a JSON file, and that JSON file is included with
